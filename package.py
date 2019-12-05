edc58b8eea6a626b6f24bdbd46df2870eeaff5c0
Bishoy <abdelmab@codescalers.com>
2019-12-05 15:35:27 +0200

    add new format

--- package.py
+++ package.py
@@ -1,53 +1,49 @@
 from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
-    """
-    to start need to run 
-    kosmos -p
-    cl = j.servers.threebot.local_start_default()
-    cl.actors.package_manager.package_add(git_url="https://github.com/enertia-io/www_enertia_io")
-    we need to restart lapis tmux
-    """
-    def _init(self, **kwargs):
-        self.branch = kwargs["package"].branch or "master"
-        self.enertia_io = "https://github.com/enertia-io/www_enertia_io.git"
+    def setup_locations(self):
+        """
+        ports & paths used for threebotserver
+        see: {DIR_BASE}/code/github/threefoldtech/jumpscaleX_core/docs/3Bot/web_environment.md
+        will start bottle server web interface which include (gedis http interface, gedis websocket interface and
+        bcdbfs web server)
+        endpoints:
+        "/web/gedis/http"       >    gedis htto interface
+        "/web/gedis/websocket"  >    gedis websocket interface
+        "/web/bcdbfs"           >    bcdbfs web server
+        "/weblibs"              >    static jumpscale weblibs files
+        """
 
-    def prepare(self):
-        """
-        called when the 3bot starts
-        :return:
-        """
-        server = self.openresty
-        server.install(reset=True)
-        server.configure()
-        website = server.get_from_port(8080)
-        locations = website.locations.get("enertia_io")
-        static_location = locations.locations_static.new()
-        static_location.name = "static"
-        static_location.path_url = "/"
-        path = j.clients.git.getContentPathFromURLorPath(self.enertia_io, branch=self.branch, pull=True)
-        static_location.path_location = path
-        static_location.use_jumpscale_weblibs = True
-        website.path = path
-        locations.configure()
-        website.configure()
+        self.openresty.configure()
+
+        # get our main webserver
+        for port in (443, 80):
+            website = self.openresty.get_from_port(port)
+
+            # PROXY for gedis HTTP
+            locations = website.locations.get(name="webinterface_locations")
+
+            package_actors_location = locations.locations_proxy.new()
+            package_actors_location.name = "enertia"
+            package_actors_location.path_url = "~* /(.*)/(.*)/actors/(.*)/(.*)$"
+            package_actors_location.ipaddr_dest = "127.0.0.1"
+            package_actors_location.port_dest = 9999
+            package_actors_location.path_dest = ""
+            package_actors_location.type = "http"
+            package_actors_location.scheme = "http"
+
+            ## more code omitted.
+
+
+            website.configure()
 
     def start(self):
-        """
-        called when the 3bot starts
-        :return:
-        """
-        self.prepare()
-    def stop(self):
-        """
-        called when the 3bot stops
-        :return:
-        """
-        pass
 
-    def uninstall(self):
-        """
-        called when the package is no longer needed and will be removed from the threebot
-        :return:
-        """
-        pass
+        # add the main webapplication
+
+        self.setup_locations()
+
+        from threebot_packages.zerobot.webinterface.bottle.gedis import app
+
+        self.gevent_rack.bottle_server_add(name="bottle_web_interface", port=9999, app=app, websocket=True)
+        # self.gevent_rack.webapp_root = webapp
