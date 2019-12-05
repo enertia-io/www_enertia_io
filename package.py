c2fb8a007ff7ef32b514fe62dd4aed76421b120b
Bishoy <abdelmab@codescalers.com>
2019-12-05 15:58:18 +0200

    test

--- package.py
+++ package.py
@@ -1,49 +1,17 @@
 from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
-    def setup_locations(self):
-        """
-        ports & paths used for threebotserver
-        see: {DIR_BASE}/code/github/threefoldtech/jumpscaleX_core/docs/3Bot/web_environment.md
-        will start bottle server web interface which include (gedis http interface, gedis websocket interface and
-        bcdbfs web server)
-        endpoints:
-        "/web/gedis/http"       >    gedis htto interface
-        "/web/gedis/websocket"  >    gedis websocket interface
-        "/web/bcdbfs"           >    bcdbfs web server
-        "/weblibs"              >    static jumpscale weblibs files
-        """
 
-        self.openresty.configure()
+    def start():
+        self.enertia_io = "https://github.com/enertia-io/www_enertia_io"
+        website = self.openresty.get_from_port(443)
+        locations = website.locations.get("enertia_io")
+        static_location = locations.locations_static.new()
+        static_location.name = "static"
+        static_location.path_url = "/"
+        path = j.clients.git.getContentPathFromURLorPath(self.enertia_io, branch=self.branch, pull=True)
+        static_location.path_location = path
+        static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
+        locations.configure()
+        website.configure()
 
-        # get our main webserver
-        for port in (443, 80):
-            website = self.openresty.get_from_port(port)
-
-            # PROXY for gedis HTTP
-            locations = website.locations.get(name="webinterface_locations")
-
-            package_actors_location = locations.locations_proxy.new()
-            package_actors_location.name = "enertia"
-            package_actors_location.path_url = "~* /(.*)/(.*)/actors/(.*)/(.*)$"
-            package_actors_location.ipaddr_dest = "127.0.0.1"
-            package_actors_location.port_dest = 9999
-            package_actors_location.path_dest = ""
-            package_actors_location.type = "http"
-            package_actors_location.scheme = "http"
-
-            ## more code omitted.
-
-
-            website.configure()
-
-    def start(self):
-
-        # add the main webapplication
-
-        self.setup_locations()
-
-        from threebot_packages.zerobot.webinterface.bottle.gedis import app
-
-        self.gevent_rack.bottle_server_add(name="bottle_web_interface", port=9999, app=app, websocket=True)
-        # self.gevent_rack.webapp_root = webapp
