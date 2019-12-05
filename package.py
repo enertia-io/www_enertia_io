190e623b4ab8b9297639ab92a3b9cb3c39eca2d1
Pishoy <pishoyabdo@gmail.com>
2019-12-05 16:16:15 +0200

    Update package.py

--- package.py
+++ package.py
@@ -12,7 +12,7 @@ class Package(j.baseclasses.threebot_package):
         locations = website.locations.get("enertia_io")
         static_location = locations.locations_static.new()
         static_location.name = "static"
-        static_location.path_url = "/enertia"
+        static_location.path_url = "/"
         path = j.clients.git.getContentPathFromURLorPath(self.enertia_io, branch=self.branch, pull=True)
         static_location.path_location = path
         static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
